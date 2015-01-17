ab77fcea7f20af04235fdeff72e585768d77d664
iveta <paneva.iveta@gmail.com>
2015-01-17 23:51:45 +0200

    Homework

---
+++ Intro-Programming-Homework/LongSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongSequence
+{
+    class LongSequence
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 1001; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + ",");
+                }
+                else
+                {
+                    Console.Write(-i);
+                    if (i < 1001)
+                    {
+                        Console.Write(",");
+                    }
+                }
+            }
+        }
+    }
+}
